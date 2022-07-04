94b512fd59c2918084bedbbf68c8a789479e7175
Keshav Sharma <sharmakeshav0708@gmail.com>
2022-07-04 16:33:09 +0530

    toggle dark theme changes

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+const swapBtn = document.getElementById('swap');
 
+function swapTheme() {
+    swapBtn.addEventListener('click', function() {
+        document.getElementById('swap').className = "button_night";
+        document.getElementById('app').className = "night";
+    })
+}
+
+swapTheme();
